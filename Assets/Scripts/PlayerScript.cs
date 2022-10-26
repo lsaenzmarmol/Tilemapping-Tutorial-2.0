c4fdef372e48be47a1d6b7b0fb9c63744385bd6f
lsaenzmarmol <lsaenzmarmol@gmail.com>
2022-10-25 23:31:38 -0400

    second commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,14 +7,21 @@ public class PlayerScript : MonoBehaviour
 {
 
     private Rigidbody2D rd2d;
+    public Text livesText;
     public float speed;
     public Text score;
-    private int scoreValue = 0;
+    private int scoreValue;
+    private int lives;
+    Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
+        lives = 3;
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString(); 
+        score.text = scoreValue.ToString();
+        livesText.text = "Lives:" + lives.ToString();
+        anim= GetComponent<Animator>(); 
     }
 
     // Update is called once per frame
@@ -27,7 +34,6 @@ public class PlayerScript : MonoBehaviour
 
     }
     
-    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.collider.tag == "Coin")
@@ -36,8 +42,28 @@ public class PlayerScript : MonoBehaviour
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject); 
         }
+        if(collision.collider.tag =="Enemy")
+        {
+            lives -= 1;
+            livesText.text = "Lives:" + lives.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+        if(lives == 0)
+        {
+            gameObject.SetActive(false);
+        }
+        if(scoreValue == 4)
+        { 
+            scoreValue = 0;
+            lives = 3;
+            score.text = scoreValue.ToString();
+            livesText.text = "Lives:" + lives.ToString();
+            transform.position = new Vector3(70.0f,0.0f,0.0f);
 
-    }
+        }
+
+    } 
+
 
     private void OnCollisionStay2D(Collision2D collision)
     {
@@ -49,5 +75,4 @@ public class PlayerScript : MonoBehaviour
             }
         }
     }
-    
 }
